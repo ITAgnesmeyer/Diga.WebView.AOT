7ad44f39a3dc6225f64e876a406cb34a1a0ce2f1
ITAgnesmeyer <guido.agnesmeyer@itagnesmeyer.de>
2025-12-03 00:57:04 +0100

    Diga.WebView2.Interop First Commit

---
+++ Diga.WebView2.Interop/ICoreWebView2AcceleratorKeyPressedEventArgs.cs
@@ -0,0 +1,34 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: Diga.WebView2.Interop.ICoreWebView2AcceleratorKeyPressedEventArgs
+// Assembly: Diga.WebView2.Interop, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: 1E8B0323-528E-4C9C-8FF8-A486637C87E1
+// Assembly location: O:\webview2\V1096133\Diga.WebView2.Interop.dll
+
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.Marshalling;
+
+namespace Diga.WebView2.Interop
+{
+    [Guid("9F760F8A-FB79-42BE-9990-7B56900FA9C7")]
+    [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    [GeneratedComInterface]
+    public partial interface ICoreWebView2AcceleratorKeyPressedEventArgs
+    {
+        //[MethodImpl(MethodImplOptions.InternalCall, MethodCodeType = MethodCodeType.Runtime)]
+        COREWEBVIEW2_KEY_EVENT_KIND GetKeyEventKind();
+
+        //[MethodImpl(MethodImplOptions.InternalCall, MethodCodeType = MethodCodeType.Runtime)]
+        uint GetVirtualKey();
+
+        //[MethodImpl(MethodImplOptions.InternalCall, MethodCodeType = MethodCodeType.Runtime)]
+        int GetKeyEventLParam();
+
+        //[MethodImpl(MethodImplOptions.InternalCall, MethodCodeType = MethodCodeType.Runtime)]
+        COREWEBVIEW2_PHYSICAL_KEY_STATUS GetPhysicalKeyStatus();
+        //[MethodImpl(MethodImplOptions.InternalCall, MethodCodeType = MethodCodeType.Runtime)]
+        int GetHandled();
+        //[MethodImpl(MethodImplOptions.InternalCall, MethodCodeType = MethodCodeType.Runtime)]
+        void SetHandled(int value);
+    }
+}
---
+++ Diga.WebView2.Interop/ICoreWebView2WindowFeatures.cs
@@ -0,0 +1,48 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: Diga.WebView2.Interop.ICoreWebView2WindowFeatures
+// Assembly: Diga.WebView2.Interop, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: 1E8B0323-528E-4C9C-8FF8-A486637C87E1
+// Assembly location: O:\webview2\V1096133\Diga.WebView2.Interop.dll
+
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.Marshalling;
+
+namespace Diga.WebView2.Interop
+{
+  [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+  [Guid("5EAF559F-B46E-4397-8860-E422F287FF1E")]
+  [GeneratedComInterface]
+  public partial interface ICoreWebView2WindowFeatures
+  {
+        //[MethodImpl(MethodImplOptions.InternalCall, MethodCodeType = MethodCodeType.Runtime)]
+        int GetHasPosition();
+
+        //[MethodImpl(MethodImplOptions.InternalCall, MethodCodeType = MethodCodeType.Runtime)]
+        int GetHasSize();
+
+        //[MethodImpl(MethodImplOptions.InternalCall, MethodCodeType = MethodCodeType.Runtime)]
+        uint Getleft();
+
+        //[MethodImpl(MethodImplOptions.InternalCall, MethodCodeType = MethodCodeType.Runtime)]
+        uint Gettop();
+
+        //[MethodImpl(MethodImplOptions.InternalCall, MethodCodeType = MethodCodeType.Runtime)]
+        uint GetHeight();
+
+        //[MethodImpl(MethodImplOptions.InternalCall, MethodCodeType = MethodCodeType.Runtime)]
+        uint GetWidth();
+
+        //[MethodImpl(MethodImplOptions.InternalCall, MethodCodeType = MethodCodeType.Runtime)]
+        int GetShouldDisplayMenuBar();
+
+        //[MethodImpl(MethodImplOptions.InternalCall, MethodCodeType = MethodCodeType.Runtime)]
+        int GetShouldDisplayStatus();
+
+        //[MethodImpl(MethodImplOptions.InternalCall, MethodCodeType = MethodCodeType.Runtime)]
+        int GetShouldDisplayToolbar();
+
+        //[MethodImpl(MethodImplOptions.InternalCall, MethodCodeType = MethodCodeType.Runtime)]
+        int GetShouldDisplayScrollBars();
+    }
+}
