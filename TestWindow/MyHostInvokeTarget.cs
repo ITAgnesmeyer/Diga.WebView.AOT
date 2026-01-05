0c04c6ee9500d869e9cd91bb75769ab5e30572e9
ITAgnesmeyer <guido.agnesmeyer@itagnesmeyer.de>
2026-01-05 12:39:48 +0100

    DLL verschoben, Binärdateien entfernt, Refactoring HostInvoke
    Die native DLL "Diga.WebView2.Native.dll" wurde ins Verzeichnis "..\Native\x64\" verschoben und die Projektdateien angepasst, sodass die DLL weiterhin ins Ausgabeverzeichnis kopiert wird. Die Klasse "MyHostInvokeTarget" wurde aus Program.cs ausgelagert und in eine eigene Datei verschoben. Nicht mehr benötigte Binärdateien (.dll, .exp, .lib, .pdb) wurden entfernt.

---
+++ TestWindow/MyHostInvokeTarget.cs
@@ -0,0 +1,20 @@
+﻿using Diga.WebView2.Interop;
+using System.Runtime.InteropServices.Marshalling;
+
+namespace TestWindow
+{
+    [GeneratedComClass]
+    public partial class MyHostInvokeTarget : IHostInvokeTarget
+    {
+        public int InvokeByName(
+            string memberName,
+            ushort dispatchFlags,
+            string jsonArgs,
+            out string bstrResult)
+        {
+            // DispatchFlags auswerten, JSON parsen, Methode/Property ausführen
+            bstrResult = "{\"ok\":true,\"result\":3}";
+            return 0; // S_OK
+        }
+    }
+}
--- TestWindow/Program.cs
+++ TestWindow/Program.cs
@@ -1,25 +1,9 @@
 ﻿using CoreWindowsWrapper;
-using Diga.WebView2.Interop;
 using System.Diagnostics;
 using System.Resources;
-using System.Runtime.InteropServices.Marshalling;
 
 namespace TestWindow
 {
-    [GeneratedComClass]
-    public partial class MyHostInvokeTarget : IHostInvokeTarget
-    {
-        public int InvokeByName(
-            string memberName,
-            ushort dispatchFlags,
-            string jsonArgs,
-            out string bstrResult)
-        {
-            // DispatchFlags auswerten, JSON parsen, Methode/Property ausführen
-            bstrResult = "{\"ok\":true,\"result\":3}";
-            return 0; // S_OK
-        }
-    }
 
     internal class Program
     {
